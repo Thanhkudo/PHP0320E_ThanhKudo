ce3d715e622179234bff3527ab5d14524fc9e614
Do Nhu Thanh <thanhkudo1o11998@gmail.com>
2020-07-15 01:56:21 +0700

    Bai tap ngay 21 +22

---
+++ Lab21/update.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+require_once 'database.php';
+if (!isset($_GET['id'])|| !is_numeric($_GET['id'])){
+    $_SESSION['error']="ID  không hợp lệ !";
+    header('Location:index.php');
+    exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Creatr </title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <style>
+        .container{
+            margin-top: 100px;
+        }
+        .btn-light a{
+            text-decoration: none;
+            color: #000000;
+            display: block;
+        }
+    </style>
+</head>
+<body>
+<?php
+$id=$_GET['id'];
+$select= "select * from employees WHERE id=$id";
+$query = mysqli_query($conn,$select);
+$row = mysqli_fetch_row($query);
+$error='';
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $des = $_POST['des'];
+    $salary = $_POST['salary'];
+    //$salary= substr('$sal','0','4');
+    $gender = $_POST['gender'];
+    $date = $_POST['date'];
+    if(empty($name)){
+        $error='Vui lòng nhập tên của bạn';
+    }
+
+    if (empty($error)){
+        $update="UPDATE `employees` SET `name`='$name',`description`='$des',`gender`='$gender',`salary`='$salary',`birthday`='$date' WHERE id=$id";
+        $is_update=mysqli_query($conn,$update);
+        if ($is_update){
+            $_SESSION['success']='Cập nhật thành công!';
+            header('Location:index.php');
+            exit();
+        }
+        else{
+            $error='Lỗi kết lối!';
+        }
+    }
+}
+?>
+<div class="container col-4">
+    <div class="form-group">
+        <form action="" method="post">
+            <h3>Update Record</h3>
+            <hr>
+            <p>Please edit input values and submit to update the record</p>
+            <label for="name">Name <span style="color: red">*</span></label>
+            <input type="text" name="name" id="name" class="form-control" value="<?php echo $row['1']?>">
+            <label for="name">Description </label>
+            <input type="text" name="des" id="des" class="form-control" value="<?php echo $row['2']?>">
+            <label for="name">Salary </label>
+            <input type="text" name="salary" id="salary" class="form-control" value="<?php echo $row['4']." VNĐ"?>">
+            <label>Gender</label><br>
+            <?php if ($row['3']==1):?>
+                <input type="radio" name="gender" id="male" value="1" class="custom-radio" checked>
+                <label for="">Male </label>
+                <input type="radio" name="gender" id="female" value="0" class="custom-radio">
+                <label for="">Female </label><br>
+            <?php else:?>
+                <input type="radio" name="gender" id="male" value="1" class="custom-radio" >
+                <label for="">Male </label>
+                <input type="radio" name="gender" id="female" value="0" class="custom-radio" checked>
+                <label for="">Female </label><br>
+            <?php endif;?>
+            <label for="name">Birthday</label>
+            <input type="date" name="date" id="date" class="form-control" value="<?php echo $row['5']?>"><br>
+            <input type="submit" name="submit" id="submit" value="Save" class="btn btn-primary">
+            <button class="btn btn-light"><a href="index.php">Cancel</a></button>
+            <h3 style="color: red"><?php echo $error?></h3>
+        </form>
+    </div>
+</div>
+</body>
+</html>
---
+++ Lab21/view.php
@@ -0,0 +1,102 @@
+<?php
+session_start();
+require_once 'database.php';
+if (!isset($_GET['id'])|| !is_numeric($_GET['id'])){
+    $_SESSION['error']="ID  không hợp lệ !";
+    header('Location:index.php');
+    exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Creatr </title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <style>
+        .container{
+            margin-top: 100px;
+        }
+        .btn a{
+            text-decoration: none;
+            color: #ffffff;
+            display: block;
+
+        }
+    </style>
+</head>
+<body>
+<?php
+$id=$_GET['id'];
+$select= "select * from employees WHERE id=$id";
+$query = mysqli_query($conn,$select);
+//$row = mysqli_fetch_row($query);
+
+?>
+<div class="container col-3">
+    <div class="form-group">
+        <h3>Update Record</h3>
+        <hr>
+<table>
+    <?php while ($row = mysqli_fetch_row($query)):?>
+        <tr>
+            <th scope="col">ID</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['0']?></td>
+        </tr>
+        <tr>
+            <th scope="col">Name</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['1']?></td>
+        </tr>
+        <tr>
+            <th scope="col">Description</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['2']?></td>
+        </tr>
+        <tr>
+            <th scope="col">Gender</th>
+        </tr>
+        <tr>
+            <?php if($row['3']==1):?>
+            <td>Male</td>
+            <?php else:?>
+            <td>Female</td>
+            <?php endif;?>
+        </tr>
+        <tr>
+            <th scope="col">Salary</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['4']?></td>
+        </tr>
+        <tr>
+            <th scope="col">Birthday</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['5']?></td>
+        </tr>
+        <tr>
+            <th scope="col">Created_at</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['6']?></td>
+        </tr>
+    <?php endwhile; ?>
+
+</table>
+        <br>
+        <button class="btn btn-primary"><a href="index.php">Back</a></button>
+    </div>
+</div>
+</body>
+</html>
